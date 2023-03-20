bbc0329df750f7963919f66e03dbd63c21ba6103
Caitlin Malloch <caitlin.malloch@gmail.com>
2023-03-20 14:17:41 -0700

    refactoring with redux toolkit

---
+++ JavaScript/Redux/Expense-Tracker/budgetSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = [
+  "housing",
+  "food",
+  "transportation",
+  "utilities",
+  "clothing",
+  "healthcare",
+  "personal",
+  "education",
+  "entertainment",
+];
+const initialState = CATEGORIES.map((category) => ({
+  category: category,
+  amount: 0,
+}));
+
+const budgetsSlice = createSlice({
+  name: budgets,
+  initialState: initialState,
+  reducers: {
+    editBugdet: (state, action) => {
+      const category = action.payload.category;
+      const amount = action.payload.amount;
+      state.find((budget) => budget.category === category).amount = amount;
+    },
+  },
+});
+
+export const selectBudgets = (state) => state.budgets;
+
+export default budgetsSlice.reducer;
+export const { editBudget } = budgetsSlice.actions;
