3babb7035397c6b5009af327577de47e6308ab0f
Caitlin Malloch <caitlin.malloch@gmail.com>
2023-03-20 12:27:42 -0700

    implement card match and flip functionality, score

--- JavaScript/Redux/Matching-Memory/src/index.js
+++ JavaScript/Redux/Matching-Memory/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { store } from "./app/store.js";
-// Add import statement below
+import { Provider } from "react-redux;";
 
 ReactDOM.render(
-  // Implement Provider component with store below
-
-  <App />,
+  <Provider store={store}>
+    <App />
+  <Provider />,
   document.getElementById("root")
 );
