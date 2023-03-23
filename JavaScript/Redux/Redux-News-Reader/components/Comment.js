ccbd9419276f01f3db0ea9d720ac86fb6185dead
Caitlin Malloch <caitlin.malloch@gmail.com>
2023-03-23 10:20:13 -0700

    createAsyncThunk, extraReducers added

--- JavaScript/Redux/Redux-News-Reader/components/Comment.js
+++ JavaScript/Redux/Redux-News-Reader/components/Comment.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loadCommentsForArticleId,
+  selectComments,
+  isLoadingComments,
+} from "../comments/commentsSlice.js";
+import { selectCurrentArticle } from "../currentArticle/currentArticleSlice.js";
 
 export default function Comment({ comment }) {
   const { id, text } = comment;
